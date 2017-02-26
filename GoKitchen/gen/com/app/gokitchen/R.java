e613b443ecfdf824bdb4898bcbd861b768edf1d3
Marc Bayón <mb154@hotmail.com>
2017-02-26 21:13:20 +0100

    Added some buttons to the main screen.

--- GoKitchen/gen/com/app/gokitchen/R.java
+++ GoKitchen/gen/com/app/gokitchen/R.java
@@ -37,7 +37,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int splash_logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int action_settings=0x7f090005;
+        public static final int button1=0x7f090000;
+        public static final int button2=0x7f090004;
+        public static final int button3=0x7f090003;
+        public static final int textView1=0x7f090001;
+        public static final int textView2=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
