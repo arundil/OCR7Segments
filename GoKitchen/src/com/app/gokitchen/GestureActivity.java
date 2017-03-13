4cf1ff1de0f50eb70de67203894ac07ac0528ad0
Marc Bayón <mb154@hotmail.com>
2017-03-13 22:50:37 +0100

    Beginning the implementation of the gesture activity.

--- GoKitchen/src/com/app/gokitchen/GestureActivity.java
+++ GoKitchen/src/com/app/gokitchen/GestureActivity.java
@@ -2,16 +2,32 @@ package com.app.gokitchen;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.util.Log;
+import android.view.GestureDetector.OnDoubleTapListener;
+import android.view.GestureDetector.OnGestureListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.OverScroller;
 
-public class GestureActivity extends Activity {
+public class GestureActivity extends Activity implements  OnGestureListener,OnDoubleTapListener {
+
+	private static final String DEBUG_TAG = "GoKitchen::GestureActivity";
+	private GestureDetectorCompat mDetector;
+	//private RectF mCurrentViewport = new RectF(AXIS_X_MIN, AXIS_Y_MIN, AXIS_X_MAX, AXIS_Y_MAX);
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gesture);
+		mDetector = new GestureDetectorCompat(this,this);
+		mDetector.setOnDoubleTapListener(this);	
 	}
 
 	@Override
@@ -40,5 +56,65 @@ public class GestureActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+
+	@Override
+	public boolean onSingleTapConfirmed(MotionEvent e) {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + e.toString());
+		return true;
+	}
+
+	@Override
+	public boolean onDoubleTap(MotionEvent e) {
+		Log.d(DEBUG_TAG, "onDoubleTap: " + e.toString());
+		return true;
+	}
+
+	@Override
+	public boolean onDoubleTapEvent(MotionEvent e) {
+		Log.d(DEBUG_TAG, "onDoubleTapEvent: " + e.toString());
+		return false;
+	}
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		 Log.d(DEBUG_TAG,"onDown: " + e.toString());
+		 return true;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+		Log.d(DEBUG_TAG, "onShowPress: " + e.toString());		
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		Log.d(DEBUG_TAG, "onSingleTapUp: " + e.toString());
+		return true;
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+		Log.d(DEBUG_TAG, "onScroll: " + e1.toString()+e2.toString());
+		return true;
+	}
+
+	@Override
+	public void onLongPress(MotionEvent e) {
+		Log.d(DEBUG_TAG, "onLongPress: " + e.toString());		
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Log.d(DEBUG_TAG, "onFling: " + e1.toString()+e2.toString());
+        return true;
 	}
 }
